df005217a842c320c887c231123dc49cc8fb955e
MrNinja008 <79526396+MrNinja008@users.noreply.github.com>
2021-08-08 12:09:34 +0530

    Create Book.php

---
+++ src/MrNinja008/infoBook/Book.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace MrNinja008\InfoBook;
+
+use pocketmine\item\Item;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Book extends PluginBase implements Listener {
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+  }
+  
+  public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        if($player->getFirstPlayed() == $player->getLastPlayed()) {
+        $book = Item::get(387,0,1);
+        $book->setCustomName("§r".$this->getConfig()->get("BookName"));
+        $book->setPageText(0,$this->getConfig()->get("PageText"));
+        $book->setAuthor($this->getConfig()->get("AuthorName"));
+        $player->getInventory()->setItem($this->getConfig()->get("BookInvSlot"), $book, true);
+       }
+    }
+ }
