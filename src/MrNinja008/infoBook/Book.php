b859c25b2da70ba7d6db42b067eee57e98dc171b
WolfDen133 <61922721+WolfDen133@users.noreply.github.com>
2021-08-15 17:21:49 +0100

    Code tidy
    Removed unnecessary code 
    Made code tidy

--- src/MrNinja008/infoBook/Book.php
+++ src/MrNinja008/infoBook/Book.php
@@ -2,6 +2,7 @@
 
 namespace MrNinja008\infoBook;
 
+use pocketmine\Player;
 use pocketmine\utils\Config;
 use pocketmine\item\WrittenBook;
 use pocketmine\plugin\PluginBase;
@@ -10,29 +11,37 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\TextFormat;
 
 class Book extends PluginBase implements Listener {
+
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        @mkdir($this->getDataFolder());
-        $this->saveResource("player.yml");
-        $this->player = new Config($this->getDataFolder() . "player.yml", Config::YAML);
+
         $this->saveDefaultConfig();
     }
 
     public function onJoin(PlayerJoinEvent $event) {
+
         $player = $event->getPlayer();
         $config = new Config($this->getDataFolder().'player.yml', Config::YAML);
+
         if(!$config->exists($player->getName())){
+
             $config->set($player->getName());
             $config->save();
-            $book = new WrittenBook();
-            $book->setCustomName(TextFormat::WHITE.$this->getConfig()->get("BookName"));
-            $contents = $this->getConfig()->get("BookContents");
-            for ($page = 0; $page < count($contents); $page++) {
-                $book->setPageText($page, $contents[$page]);
-            }
 
-            $book->setAuthor($this->getConfig()->get("AuthorName"));
-            $player->getInventory()->setItem($this->getConfig()->get("BookInvSlot"), $book, true);
+            $book = new WrittenBook();
+            $book->setCustomName(TextFormat::colorize($this->getConfig()->get("BookName")));
+            $book->setAuthor(TextFormat::colorize($this->getConfig()->get("AuthorName")));
+
+            $contents = $this->getConfig()->get("BookContents");
+
+            foreach ($contents as $index=>$content) $book->setPageText($index, $this->replaceTags($content[$index], $player));
+
+            $player->getInventory()->setItem((int)$this->getConfig()->get("BookInvSlot"), $book, true);
         }
+    }
+
+    public function replaceTags (string $string, Player $player) : string
+    {
+        return TextFormat::colorize(str_replace(["{max_players}", "{online_players}", "{player}","{ping}" ], [$this->getServer()->getMaxPlayers(), (string) count($this->getServer()->getOnlinePlayers()), $player->getName(), (string) $player->getPing()], $string));
     }
 }
